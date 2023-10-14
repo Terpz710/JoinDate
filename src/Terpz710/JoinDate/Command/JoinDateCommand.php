3e695cd5b38cf570676ed71dbb9e72c69482acf4
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-10-14 15:01:23 -0700

    Update JoinDateCommand.php

--- src/Terpz710/JoinDate/Command/JoinDateCommand.php
+++ src/Terpz710/JoinDate/Command/JoinDateCommand.php
@@ -25,18 +25,18 @@ class JoinDateCommand extends Command implements PluginOwned {
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-    if ($sender instanceof Player) {
-        $targetPlayer = $this->plugin->getServer()->getPlayerByXuid($sender->getXuid()); // Get the player instance
-        if ($targetPlayer instanceof Player) {
-            $firstJoinDate = $targetPlayer->getFirstPlayed();
-            $formattedDate = date("Y-m-d h:i A", $firstJoinDate);
-            $sender->sendMessage("Your join date: " . $formattedDate);
+        if ($sender instanceof Player) {
+            $targetPlayer = $this->plugin->getServer()->getPlayerByXuid($sender->getXuid()); // Get the player instance
+            if ($targetPlayer instanceof Player) {
+                $firstJoinDate = $targetPlayer->getFirstPlayed();
+                $formattedDate = date("Y-m-d h:i A", $firstJoinDate);
+                $sender->sendMessage("Your join date: " . $formattedDate);
+            } else {
+                $sender->sendMessage("Player not found or is offline.");
+            }
         } else {
-            $sender->sendMessage("Player not found or is offline.");
+            $sender->sendMessage("This command can only be used in-game.");
         }
-    } else {
-        $sender->sendMessage("This command can only be used in-game.");
-    }
-    return true;
+        return true;
     }
 }
